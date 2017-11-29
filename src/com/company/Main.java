79376d0b0c4f713b819a2c816b061bc5ee6e7ff0
kd944 <deleon_k@ymail.com>
2017-11-29 08:28:32 -0500

    my method is weird sos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String t1 = cat(68, 108);
+        System.out.println(t1);
+
+        String t2 = cat(35, 59);
+        System.out.println(t2);
+
     }
+
+    public String cat(int s, int e){
+        int startPos = s;
+        int endPos = e;
+
+        char startChar = (char)startPos;
+
+
+        StringBuffer ss = new StringBuffer(startChar);
+        for(int j = startPos; j < endPos; startChar++){
+            ss.append(startChar + 1);
+        }
+
+        String sa = ss.toString();
+
+        return sa;
+
+    }
+
 }
